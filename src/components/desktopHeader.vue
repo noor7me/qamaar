4a09917b20ed0eb8d4a1415713a796d9d03c8b10
Abdinur Mohamed <noor1me@uw.edu>
2019-12-31 18:34:19 -0800

    toolbar added

--- src/components/desktopHeader.vue
+++ src/components/desktopHeader.vue
@@ -3,37 +3,104 @@
         app
         color="primary"
         dark
+        flat
+        dense
+        
     >
     <div class="d-flex align-center">
-    <v-img
-        alt="QAMAAR Logo"
-        class="shrink mr-2"
-        contain
-        src="/assets/BIGQ.jpg"
-        transition="scale-transition"
-        width="40"
-    />
+        <v-img
+            alt="QAMAAR Logo"
+            class="shrink mr-2"
+            contain
+            src="/assets/BIGQ.jpg"
+            transition="scale-transition"
+            width="40"
+        >
+        </v-img>
+       
+    </div>
+    <div    class="menu-box"
+            v-if="!searchOn" >
+        <span class="indigo--text">Menu Should appear </span>
+    </div>
+    <div v-if="searchOn" class="search-box"> 
+        <v-text-field
 
+            v-model="message"
+            outlined
+            filled
+            solo-inverted
+            @click:clear="onClose()"
+            dense
+            flat
+            clearable
+            label="Search Qamaar Store"
+            type="text"
+            color="success"
+            prepend-inner-icon="mdi-magnify"
+            @click:prepend-inner="onClose()"
+            @keyup.enter="searchOn = !searchOn" 
+               
+        >
+        </v-text-field>
     </div>
 
     <v-spacer></v-spacer>
 
     <v-btn icon>
-        <v-icon color="black">mdi-magnify</v-icon>
+        <v-icon color="success"
+            @click="searchOn = !searchOn"
+        >mdi-magnify</v-icon>
     </v-btn>
 
     <v-btn icon>
-        <v-icon color="black">mdi-cart</v-icon>
+        <v-icon color="success">mdi-cart</v-icon>
     </v-btn>
     </v-app-bar>
 </template>
 
 <script>
 export default {
+    name: 'desktopHeader',
+    data() {
+        return {
+            searchOn: false,
+            message: "",
+            loading: false
 
+        }
+    }, 
+    methods: {
+        clickMe()   {
+            this.loading    = true,
+            this.message    =   "wait for it"
+            setTimeout(() => {
+                this.loading    =   false,
+                this.message    = " i am done !!!"
+            }, 2000)
+        },
+        clickMe2() {
+            this.searchOn = false
+        },
+
+        onClose() {
+            this.searchOn = !this.searchOn; //close the search box 
+            this.message  = "";              // empty the search box 
+        }
+    }
+    
 }
 </script>
 
-<style>
+<style scoped>
+    .search-box{
+        margin-left: 500px;
+        margin-top: 30px;
+        width: 800px;
+        border-radius: 120px;
+    }
+    .menu-box{
+        margin-left: 200px;
+    }
 
 </style>
--- src/components/mobileHeader.vue
+++ src/components/mobileHeader.vue
@@ -4,46 +4,115 @@
             app
             dark
             color="primary"
+            dense
         >
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer" color="black"></v-app-bar-nav-icon>
-            <div class="d-flex align-center">
-                <v-img
-                    alt="QAMAAR Logo"
-                    class="shrink mr-2"
-                    contain
-                    src="/assets/BIGQ.jpg"
-                    transition="scale-transition"
-                    width="20"
-                />
-            </div>
-            <v-spacer></v-spacer>
-            
-            <v-btn icon>
-                <v-icon color="black">mdi-magnify</v-icon>
-            </v-btn>
+       
+            <v-app-bar-nav-icon 
+              @click.stop="drawer = !drawer" 
+              color="success"
+              v-show="!searchOn"
+              >
+              </v-app-bar-nav-icon>
+           
+              <div class="d-flex align-center">
+                  <v-img
+                      v-show="!searchOn"
+                      alt="QAMAAR Logo"
+                      class="shrink mr-2"
+                      contain
+                      src="/assets/BIGQ.jpg"
+                      transition="scale-transition"
+                      width="20"
+                  />
+              </div>
+                <v-spacer></v-spacer>
+                <v-text-field
+                    v-show="searchOn"
+                    type="text"
+                    outlined
+                    filled
+                    flat
+                    clearable
+                    dense
+                    solo-inverted
+                    color="success"
+                    prepend-inner-icon="mdi-magnify"
+                  
+                    label="Search Qamaar store"
+                    class="search-box"
 
-            <v-btn icon>
-                <v-icon color="black">mdi-cart</v-icon>
-            </v-btn>     
+
+                  >
+
+                </v-text-field>
+         
+            <div class="my-2"> 
+              <v-btn
+                v-show="searchOn"
+                tile
+                color="primary black--text"
+                elevation = "0"
+                depressed
+
+                exact
+                width="10px"
+                height="40px"
+                @click="closeSearch()"
+                >
+                <span class="cancel-text">Cancel</span>
+                
+              </v-btn> 
+              </div>           
+            <div v-show="!searchOn">
+              <v-btn icon>
+                  <v-icon color="success"
+                    @click="closeSearch()"
+                  >mdi-magnify</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                  <v-icon color="success">mdi-cart</v-icon>
+              </v-btn>
+            </div>
         </v-app-bar>
     </div>
 </template>
 
 <script>
   export default {
-    data: () => ({
-      drawer: false,
-      group: null,
-    }),
+    name: 'mobileHeader',
+    data() {
+      return{
+        drawer: false,
+        group: false,
 
+        searchOn: false
+      }
+    },
+
+  methods: {
+    closeSearch() {
+      this.searchOn = !this.searchOn    //exit search mode
+    },
+  },
     watch: {
       group () {
         this.drawer = false
+        },
       },
-    },
-  }
+    }
 </script>
 
-<style>
+<style scoped>
+  .search-box{
+    margin-top: 28px;
+    margin-right: 10px;
+    width: 240px;
+  }
+  .cancel-text{     
+    font-size: 10px;
+    font-style: normal;
+  }
+
 
 </style>
